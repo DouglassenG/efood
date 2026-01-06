c5ea3bdee24f7cbe5d9e81f9f7d4e9f60446e479
Douglas <douglasmsrodrigues@hotmail.com>
2026-01-05 21:14:09 -0300

    Implementando o banner como elemento.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,19 @@
-import apresentacao from '../../assets/images/apresentacao.png'
-import imagemHeader from '../../assets/images/imagem_header.png'
-import { ApresentacaoContainer, HeaderContainer } from './styles'
+import { Link } from 'react-router-dom'
+import { HeaderContainer, Titulo } from './styles'
+
+import logo from '../../assets/images/logo.png'
+import fundo from '../../assets/images/fundo.png'
 
 const Header = () => (
-  <>
-    <HeaderContainer
-      style={{ backgroundImage: `url(${imagemHeader})` }}
-    ></HeaderContainer>
-    <ApresentacaoContainer
-      style={{ backgroundImage: `url(${apresentacao})` }}
-    ></ApresentacaoContainer>
-  </>
+  <HeaderContainer style={{ backgroundImage: `url(${fundo})` }}>
+    <div className="container">
+      <Link to="/">
+        <Titulo>Restaurantes</Titulo>
+      </Link>
+      <img src={logo} alt="EFOOD" />
+      <Titulo>0 produto(s) no carrinho</Titulo>
+    </div>
+  </HeaderContainer>
 )
 
 export default Header
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,19 +1,32 @@
 import styled from 'styled-components'
+import { cores } from '../../styles'
 
 export const HeaderContainer = styled.div`
   width: 100%;
   height: 163px;
-  display: block;
-  text-align: center;
+  display: flex;
+  align-items: center;
   background-repeat: no-repeat;
-  background-position: center center;
+  background-size: cover;
+  background-position: center;
+
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1024px;
+    width: 100%;
+    margin: 0 auto;
+  }
+
+  a {
+    text-decoration: none;
+  }
 `
 
-export const ApresentacaoContainer = styled.div`
-  width: 100%;
-  height: 280px;
-  display: flex;
-  text-align: center;
-  background-repeat: no-repeat;
-  background-position: center center;
+export const Titulo = styled.h2`
+  font-weight: 900;
+  font-size: 18px;
+  line-height: 21px;
+  color: ${cores.laranja};
 `
