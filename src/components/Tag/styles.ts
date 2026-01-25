1cbce80aa390a816dbb6211ad0836dc92ada6e73
Douglas <douglasmsrodrigues@hotmail.com>
2026-01-25 14:47:38 -0300

    Fix: corrigindo as dependencias

--- src/components/Tag/styles.ts
+++ src/components/Tag/styles.ts
@@ -1,9 +1,8 @@
 import styled from 'styled-components'
 import { cores } from '../../styles'
 
-import { Props } from '.'
 
-export const TagContainer = styled.div<Props>`
+export const TagContainer = styled.div<{ size?: 'small' | 'big' }>`
   background-color: ${cores.laranja};
   color: ${cores.laranjaClaro};
   font-size: ${(props) => (props.size === 'big' ? '14px' : '12px')};
